79301c24b8a31689b913cfd149dcacad7115b4e1
david <davidsoliss123@gmail.com>
2025-04-03 01:40:09 -0500

    images, templates, css, app.py routes

---
+++ datos.py
@@ -0,0 +1,8 @@
+productos = [
+    {"nombre": "Camara", "precio": 7000},
+    {"nombre": "Celular", "precio": 8500},
+    {"nombre": "Bateria", "precio": 5000},
+    {"nombre": "Keyboard", "precio": 280000},
+    {"nombre": "Monitor", "precio": 900000},
+    {"nombre": "Speaker", "precio": 250000},
+]
